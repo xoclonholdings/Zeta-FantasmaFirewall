de3cea72323a93710fed58affda7a691cff8c8e2
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-08-24 06:42:08 -0400

    Register FanFI governed routes

--- server/routes.ts
+++ server/routes.ts
@@ -5,6 +5,7 @@ import { registerContentRoutes } from "./routes/content";
 import { registerSecurityRoutes } from "./routes/security";
 import { registerSystemRoutes } from "./routes/system";
 import { registerIntegrityRoutes } from "./routes/integrity";
+import { registerFanfiRoutes } from "./routes/fanfi";
 
 export async function registerRoutes(app: Express): Promise<Server> {
   const httpServer = createServer(app);
@@ -15,6 +16,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
 
   registerSecurityRoutes(app);
   registerIntegrityRoutes(app);
+  registerFanfiRoutes(app);
   registerContentRoutes(app);
   registerSystemRoutes(app);
 
